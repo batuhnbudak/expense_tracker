cb9230a1ddaa0a70bef525566c1ce74788f695b3
bauthnbudak <batuhan.budak@ug.bilkent.edu.tr>
2022-06-02 20:54:34 +0300

    FIX:: .

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../ui/Card.js";
+import { useState } from "react";
+
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.expenses.title);
+
+  function onClickHandler() {
+    setTitle("updated");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expenses.date} />
       <div className="expense-item__description">
-        <h2>{props.expenses.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.expenses.amount}</div>
       </div>
+      <button onClick={onClickHandler}>Change Title</button>
     </Card>
   );
 }
